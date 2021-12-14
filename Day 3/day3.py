7a49af6e238b896065d14414697ad0850b43e637
Max Cheever <74006882+maxcheever@users.noreply.github.com>
2021-12-14 23:40:30 +0000

    added day 3 part 2

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,3 +1,4 @@
+######################### PART 1 ###########################
 def part1():
   g = gamma()
   e = toDec(epsilon(g))
@@ -45,3 +46,80 @@ def toDec(binary):
     decimal = decimal*2 + int(digit)
 
   return decimal
+
+
+######################### PART 2 ###########################
+def part2():
+  d = getReadings()
+  carbon = toDec(cScrub(d))
+  oxygen = toDec(oRate(d))
+  final = oxygen*carbon
+  
+  return final
+
+## gets all readings and returns a list of them
+def getReadings():
+  with open('inputDay3.txt', 'r') as f:
+    d = []
+    for l in f:
+      d.append(l.rstrip('\n'))
+  
+  return d
+
+## counter for the most common bit at a given index
+def counter(d, i):
+  count1 = 0
+  count0 = 0
+  for v in d:
+    if v[i] == '0':
+      count0 += 1
+    else:
+      count1 += 1
+  if count1 > count0 or count1 == count0:
+    return '1'
+  else:
+    return '0'
+
+## finds the oxygen scrubber rate in binary
+def oRate(d):
+  oBin = d
+  for i in range(0,12):
+    newBin = []
+    ## finds most common bit
+    most = counter(oBin,i)
+    ## appends every value with most common bit at index i
+    if most == '1':
+      for v in oBin:
+        if v[i] == '1':
+          newBin.append(v)
+      ## the new list only will contain values with most common occurence
+      oBin = newBin
+    ## same idea here, just if 0 is most common occurence
+    else:
+      for v in oBin:
+        if v[i] == '0':
+          newBin.append(v)
+      oBin = newBin
+
+  return oBin[0]
+
+## this is the same as the oxygen scrubber rate, just with least common bit
+def cScrub(d):
+  cBin = d
+  for i in range(0,12):
+    newBin = []
+    most = counter(cBin,i)
+    if most == '1':
+      for v in cBin:
+        if v[i] == '0':
+          newBin.append(v)
+      cBin = newBin
+    else:
+      for v in cBin:
+        if v[i] == '1':
+          newBin.append(v)
+      cBin = newBin
+    if len(cBin) == 1:
+      break
+
+  return cBin[0]
